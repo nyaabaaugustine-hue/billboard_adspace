392d3d13b26a6698b77972cba9fb7d26af22def3
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-20 21:32:24 +0000

    make logo animation , like an heartbeat

--- src/components/layout/Preloader.tsx
+++ src/components/layout/Preloader.tsx
@@ -6,13 +6,13 @@ import { motion } from 'framer-motion';
 export function Preloader() {
   return (
     <motion.div
-      initial={{ scale: 0.95, opacity: 0.8 }}
-      animate={{ scale: 1, opacity: 1 }}
+      animate={{
+        scale: [1, 1.05, 1],
+      }}
       transition={{
-        duration: 0.8,
+        duration: 1.2,
         ease: 'easeInOut',
         repeat: Infinity,
-        repeatType: 'reverse',
       }}
     >
       <OwareLogo width={128} height={32} />
