9a1191a70b6173ea997f6042d827082cce9f370e
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-20 21:04:49 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -33,6 +33,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
